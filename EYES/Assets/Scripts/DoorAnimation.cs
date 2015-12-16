9664e97f39d7e0666ca82284e815e9171c823f0b
Stefan Büttner <buettner.stefan@web.de>
2015-12-16 10:27:15 +0100

    [Explorationslevel] Create initial verion

---
+++ EYES/Assets/Scripts/DoorAnimation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorAnimation : MonoBehaviour {
+
+    private Animator[] anims;
+    private GameObject player;
+    private int count = 0;
+
+    void Awake () {
+        anims = GetComponentsInChildren<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnTriggerEnter (Collider other) {
+        Debug.Log(this.gameObject.name + "::OnTriggerEnter");
+        if (other.gameObject == player) {
+            ++count;
+        }
+    }
+
+    void OnTriggerExit (Collider other) {
+        Debug.Log(this.gameObject.name + "::OnTriggerExit");
+        if (other.gameObject == player) {
+            --count;
+        }
+    }
+
+    void Update () {
+        foreach (Animator anim in anims) {
+            anim.SetBool("Open", count > 0);
+        }
+    }
+}
