b46267114d9fe861f124d0ca6fbc2294e2b8d100
Selemp <schlupyupyip@gmx.de>
2016-01-05 11:51:08 +0100

    collision + timer
    Kollision mit allen Gegenständen, und ein kleiner Timer um die Zeit zu
    zählen.

---
+++ EYES/Assets/Scripts/Timer.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var timer : float = 0.0;
+
+function Update() {
+
+	timer += Time.deltaTime;
+
+}
+
+function OnGUI() {
+	GUI.Box(new Rect(10,50,50,20), "" + timer.ToString("0"));
+	}
