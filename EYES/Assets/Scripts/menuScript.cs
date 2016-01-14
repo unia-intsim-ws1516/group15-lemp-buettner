5050b376a2d15ac3be92d26ba38b0e7feb65920d
Selemp <schlupyupyip@gmx.de>
2016-01-14 18:51:14 +0100

    Levelselect

--- EYES/Assets/Scripts/menuScript.cs
+++ EYES/Assets/Scripts/menuScript.cs
@@ -6,16 +6,22 @@ using UnityEngine.SceneManagement;
 public class menuScript : MonoBehaviour 
 {
 	public Canvas QuitMenu;
+	public Canvas Levelmenu;
 	public Button playbutton;
 	public Button quitbutton;
+	public Button levelbutton;
 
 	void Start ()
 
 	{
 		QuitMenu = QuitMenu.GetComponent<Canvas>();
+		QuitMenu.enabled = false;
+		Levelmenu = Levelmenu.GetComponent<Canvas>();
+		Levelmenu.enabled = false;
 		playbutton = playbutton.GetComponent<Button> ();
 		quitbutton = quitbutton.GetComponent<Button> ();
-		QuitMenu.enabled = false;
+		levelbutton = levelbutton.GetComponent<Button> ();
+
 
 	}
 
@@ -25,15 +31,20 @@ public class menuScript : MonoBehaviour
 		QuitMenu.enabled = true; //enable the Quit menu when we click the Exit button
 		playbutton.enabled = false; //then disable the Play and Exit buttons so they cannot be clicked
 		quitbutton.enabled = false;
-
+		levelbutton.enabled = false;
+		Levelmenu.enabled = false;
 	}
 
 	public void NoPress() //this function will be used for our "NO" button in our Quit Menu
 
 	{
-		QuitMenu.enabled = false; //we'll disable the quit menu, meaning it won't be visible anymore
+		QuitMenu.enabled = false;	//we'll disable the quit menu, meaning it won't be visible anymore
+		Levelmenu.enabled = false;
 		playbutton.enabled = true; //enable the Play and Exit buttons again so they can be clicked
 		quitbutton.enabled = true;
+		levelbutton.enabled = true;
+	
+
 
 	}
 
@@ -52,7 +63,40 @@ public class menuScript : MonoBehaviour
 	}
 	public void goMenu ()
 	{
-		SceneManager.LoadScene(0); // laod Menu.
+		SceneManager.LoadScene(0); // load Menu.
 	}
 
+	public void Levelpress ()
+		
+	{
+		Levelmenu.enabled = true; //
+		playbutton.enabled = false; //
+		quitbutton.enabled = false;
+		levelbutton.enabled = false;
+	}
+
+	public void levelColorblindness ()
+	{
+		SceneManager.LoadScene(1); // load Colorblindness
+	}
+
+	public void levelGlaucoma ()
+	{
+		SceneManager.LoadScene(1); // load Glaucoma
+	}
+
+	public void levelCataract ()
+	{
+		SceneManager.LoadScene(1); // load Cataract
+	}
+
+	public void levelMyopia ()
+	{
+		SceneManager.LoadScene(1); // load Myopia
+	}
+
+	public void levelHyperopia ()
+	{
+		SceneManager.LoadScene(1); // load Hyperopia
+	}
 }
