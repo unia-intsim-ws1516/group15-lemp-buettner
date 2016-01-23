de850e75f885220b84e0b430e79165a08428b062
Selemp <schlupyupyip@gmx.de>
2016-01-23 13:47:30 +0100

    level ending beginning

---
+++ EYES/Assets/Scripts/levelend2.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class levelend2 : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col)
+
+	{
+		if(col.gameObject.name == "Player")
+			{
+			SceneManager.LoadScene (0);
+		}
+	}
+}
